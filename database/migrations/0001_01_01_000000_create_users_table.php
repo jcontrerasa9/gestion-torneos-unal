e1ed52b5cf5a2a2feef6c1a52cb36cc60a1c0ae4
jcontrerasa9 <jacontrerasa@unal.edu.co>
2026-07-09 16:09:50 -0500

    fix: role_id en users con una migracion adicional por el orden de ejecucion

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->nullable()->constrained('roles')->nullOnDelete();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
