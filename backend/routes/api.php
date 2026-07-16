71fffc6399be3ed0202ba43e4b524ffc72a7c4dc
jcontrerasa9 <jacontrerasa@unal.edu.co>
2026-07-15 21:11:42 -0500

    feat: rutas de operacion crud para teams usando rolemiddleware y authmiddleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Team\TeamController;
 use App\Http\Controllers\User\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,12 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    Route::apiResource('teams', TeamController::class)->only(['index', 'show']);
+
+    Route::middleware('role:captain,admin')->group(function () {
+        Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
+        Route::put('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');
+        Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
+    });
 });
